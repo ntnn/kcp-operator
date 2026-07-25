2972eca7a0e95845ae2fde45cc6af09e3c4fad21
Nelo-T. Wallus <n.wallus@sap.com>
2026-07-25 14:34:02 +0200

    Add --enabled-controller-groups flag to toggle controller groups

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -31,9 +31,11 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
@@ -81,6 +83,7 @@ func run(ctx context.Context) error {
 	var (
 		metricsAddr, probeAddr                           string
 		enableLeaderElection, secureMetrics, enableHTTP2 bool
+		enabledControllerGroups                          []string
 		tlsOpts                                          []func(*tls.Config)
 	)
 
@@ -97,6 +100,9 @@ func run(ctx context.Context) error {
 		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	fs.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+	fs.StringSliceVar(&enabledControllerGroups, "enabled-controller-groups",
+		[]string{string(config.ControllerGroupConfig), string(config.ControllerGroupWorkload)},
+		"Comma-separated list of controller groups to enable (available: config, workload).")
 
 	// Add feature gates flag
 	config.DefaultMutableFeatureGate.AddFlag(fs)
@@ -117,6 +123,12 @@ func run(ctx context.Context) error {
 
 	// Log enabled feature gates
 	setupLog.Info("Feature gates", "gates", config.DefaultFeatureGate)
+
+	controllerGroups, err := config.ParseControllerGroups(enabledControllerGroups)
+	if err != nil {
+		return fmt.Errorf("invalid --enabled-controller-groups: %w", err)
+	}
+	setupLog.Info("Enabled controller groups", "groups", sets.List(controllerGroups))
 
 	log := zap.NewRaw(zap.UseFlagOptions(&opts))
 	ctrl.SetLogger(zapr.NewLogger(log))
@@ -190,56 +202,10 @@ func run(ctx context.Context) error {
 
 	client := mgr.GetClient()
 
-	if err = (&rootshard.RootShardReconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "RootShard", err)
-	}
-	if err = (&frontproxy.FrontProxyReconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "FrontProxy", err)
-	}
-	if err = (&shard.ShardReconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "Shard", err)
-	}
-	if err = (&cacheserver.CacheServerReconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "CacheServer", err)
-	}
-	if err = (&kubeconfig.KubeconfigReconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "Kubeconfig", err)
-	}
-	if err = (&virtualworkspace.Reconciler{
-		Client: client,
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		return fmt.Errorf("unable to create controller %s: %w", "VirtualWorkspace", err)
-	}
-	if config.Enabled(config.ConfigurationBundle) {
-		if err = (&bundle.BundleReconciler{
-			Client: client,
-			Scheme: mgr.GetScheme(),
-		}).SetupWithManager(mgr); err != nil {
-			return fmt.Errorf("unable to create controller %s: %w", "Bundle", err)
+	if controllerGroups.Has(config.ControllerGroupConfig) {
+		if err := setupConfigControllers(mgr, client); err != nil {
+			return err
 		}
-	}
-	if err = (&kubeconfigrbac.KubeconfigRBACReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "KubeconfigRBAC")
-		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
 
@@ -257,4 +223,59 @@ func run(ctx context.Context) error {
 
 	setupLog.Info("starting manager")
 	return mgr.Start(ctx)
+}
+
+func setupConfigControllers(mgr ctrl.Manager, client ctrlruntimeclient.Client) error {
+	if err := (&rootshard.RootShardReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "RootShard", err)
+	}
+	if err := (&frontproxy.FrontProxyReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "FrontProxy", err)
+	}
+	if err := (&shard.ShardReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "Shard", err)
+	}
+	if err := (&cacheserver.CacheServerReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "CacheServer", err)
+	}
+	if err := (&kubeconfig.KubeconfigReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "Kubeconfig", err)
+	}
+	if err := (&virtualworkspace.Reconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "VirtualWorkspace", err)
+	}
+	if config.Enabled(config.ConfigurationBundle) {
+		if err := (&bundle.BundleReconciler{
+			Client: client,
+			Scheme: mgr.GetScheme(),
+		}).SetupWithManager(mgr); err != nil {
+			return fmt.Errorf("unable to create controller %s: %w", "Bundle", err)
+		}
+	}
+	if err := (&kubeconfigrbac.KubeconfigRBACReconciler{
+		Client: client,
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("unable to create controller %s: %w", "KubeconfigRBAC", err)
+	}
+
+	return nil
 }
